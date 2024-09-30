4279ac3dd14e8ca227e4878c37b002a1e7672526
ousbahou <ousbane.houda@gmail.com>
2024-09-30 15:37:47 +0200

    test php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,23 +8,12 @@
     <link rel="stylesheet" href="navbar/navbar.css">
     <title>UIFry</title>
 </head>
+
 <body>
     <header>
-        <nav class="flex-nav">
-            <img src="./assets/logo.svg" alt="Image du logo UIFry">
-            <ul>
-                <li>About UIFry ?</li>
-                <li>Features</li>
-                <li>Ressources</li>
-                <li>Company</li>
-            </ul>
-            <div>
-                <button>Get Demo</button>
-                <a href="">Contact sales</a>
-            </div>
-    
-        </nav>
+        <?php include 'navbar.php'; ?>
     </header>
+
     <main>
         <div class="container hero">
             <div class="hero_left">
@@ -38,22 +28,31 @@
                     <p class="ratings_text">Based on <span>10,000+</span> reviews</p>
                 </div>
                 <div class="hero_blog">
-                        <span>Seamless solution<br>with our magic!</span>
-                        <p>UIFry is the project management platform that aims for teams to<br>achieve an efficient dream management</p>
+                    <span>Seamless solution<br>with our magic!</span>
+                    <p>UIFry is the project management platform that aims for teams to<br>achieve an efficient dream
+                        management</p>
                     <div class="hero_blog_demo">
                         <p>Start work efficiently with<br>UIFry SaaS product</p>
                         <button class="button button_green">Get a Free Demo</button>
                     </div>
                 </div>
                 <ul class="hero_logos">
-                    <li><img src="assets/logos/logoipsum1.png"/></li>
-                    <li><img src="assets/logos/logoipsum2.png"/></li>
-                    <li><img src="assets/logos/logoipsum3.png"/></li>
+                    <li><img src="assets/logos/logoipsum1.png" /></li>
+                    <li><img src="assets/logos/logoipsum2.png" /></li>
+                    <li><img src="assets/logos/logoipsum3.png" /></li>
                 </ul>
             </div>
             <div class="hero-left"></div>
         </div>
     </main>
-    <footer>FOOTER</footer>
+
+    <section>
+        <?php require './carroussel/index.html'; ?>
+    </section>
+
+    <section>
+    </section>
+
 </body>
+
 </html>
